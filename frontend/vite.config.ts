e11dc213e90846649a33ec862c279eb4b69e01f2
viraj07 <122121709+Viraj-rana@users.noreply.github.com>
2026-02-16 13:46:31 +0300

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,22 +3,18 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-//we can also use here build: { outDir: 'build' } to change build folder name
 export default defineConfig(({ mode }) => ({
   build: {
     outDir: 'build'
   },
+  
+  // Server configuration
   server: {
-      port: 3000,
+    port: 3000,
     host: '0.0.0.0',
     allowedHosts: true
   },
-// export default defineConfig(({ mode }) => ({
-//   server: {
-//     host: "::",
-//     port: 8080,
-//   },
+  
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
